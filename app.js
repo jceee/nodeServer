acbe3eabb2a1ae242d6daedadc29fed8c8dcba95
L3ve <370157506@qq.com>
2016-03-23 09:52:10 +0800

    添加分页参数

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ var app = module.exports = koa();
 var path = require('path');
 var url = require('url');
 var querystring = require('querystring');
-var extname = path.extname;
 
 app.use(function* () {
-    var path = __dirname + '/data' + this.path + '.json';
+    var query = url.parse(this.url).query;
+    query = querystring.parse(query);
+    var _callback = query.callback;
+    var _page = query.page ? query.page : '';
+    var path = `${__dirname}/data${this.path}${_page}.json`;
     var fstat = yield stat(path);
-
+    console.log(path);
     if (fstat.isFile()) {
-        this.type = extname(path);
-        var query = url.parse(this.url).query;
-        var _callback = querystring.parse(query).callback;
         var rs = fs.readFileSync(path,{
             "encoding": "utf8"
         });
