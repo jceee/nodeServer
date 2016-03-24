6f4653719e1ca963c66ef41d472cfb1e5c369471
L3ve <370157506@qq.com>
2016-03-24 14:40:32 +0800

    post版

--- src/app.js
+++ src/app.js
@@ -2,26 +2,35 @@ var koa = require('koa');
 var fs = require('fs');
 var app = module.exports = koa();
 var path = require('path');
+var coBody = require('co-body');
 var hp = require('./handleparam.js');
 
 
+app.use(function* (next) {
+    var _path = `./data${this.path}.json`;
+    var query = yield next;
+    //读本地json
+    var rs = fs.readFileSync(_path, {
+        "encoding": "utf8"
+    });
+    console.log(query);
+    this.body = hp.handleparam(rs, query);
+});
+
 app.use(function* () {
-    var path = `./data${this.path}.json`;
-    var fstat = yield stat(path);
-    if (fstat.isFile()) {
-        var rs = fs.readFileSync(path,{
-            "encoding": "utf8"
-        });
-        this.body = hp.handleparam(rs,this.url);
-    }
+    return (()=>{
+        if (this.method === 'GET') {
+            return this.query;
+        } else if (this.method === 'POST') {
+            // var _body = yield coBody(this);
+            // return _body;
+        }
+    })()
+});
+
+
+app.on('error', function(err, ctx) {
+    log.error('server error', err, ctx);
 });
 
 if (!module.parent) app.listen(3333);
-
-
-//读本地json
-function stat(file) {
-    return function(done) {
-        fs.stat(file, done);
-    };
-}
--- src/handleparam.js
+++ src/handleparam.js
@@ -1,39 +1,35 @@
-var url = require('url');
-var querystring = require('querystring');
-
-
-exports.handleparam = function(txt,_url) {
-    var query = querystring.parse(url.parse(_url).query);
+exports.handleparam = function(txt, query) {
     var _callback = query.callback;
-    var res = _callback + '(' + handleJson(txt,query) + ')'
+    var res = _callback + '(' + handleJson(txt, query) + ')'
     return res;
 };
 
-function handleJson(json,query) {
-    var _page = query.page ? query.page : false;
-    var _name = query.name ? query.name.split(',') : false;
-    var _identify = query.identify ? query.identify.split(',') : false
-    var obj = sTo(json);
+function handleJson(json, query) {
+    var _page = query.page ? query.page : false,
+        _name = query.name ? query.name.split(',') : false,
+        _identify = query.identify ? query.identify.split(',') : false,
+        obj = sTo(json);
 
-    if(_identify) {
-        _identify.map((idt)=>{
+    if (_identify) {
+        _identify.map((idt) => {
             obj = obj[idt];
+            console.log(obj);
         })
     }
-    if(_page && obj instanceof Array) {
-            if (!obj[_page-1]) {
-                var errorLog = {msg: '参数page错误'};
-                return oTs(errorLog);
-            } else {
-                obj = obj[_page-1];
-            }
+    if (_page && obj instanceof Array) {
+        if (!obj[_page - 1]) {
+            var errorLog = { msg: '参数page错误' };
+            return oTs(errorLog);
+        } else {
+            obj = obj[_page - 1];
+        }
     }
-    if(_name) {
-        _name.map((name)=>{
+    if (_name) {
+        _name.map((name) => {
             try {
                 obj = obj[name];
             } catch (error) {
-                console.log('参数name的错误:'+error);
+                console.log('参数name的错误:' + error);
             }
         })
     }
